56c9888afb84b9418591fb2c82a155fdf22a72dc
zhangwanqiang <412419407@qq.com>
2020-11-06 17:02:27 +0800

    first commit

---
+++ src/main/java/com/roncoo/eshop/product/EshopProductServiceApplication.java
@@ -0,0 +1,20 @@
+package com.roncoo.eshop.product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * 商品服务
+ * @author 41241
+ *
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class EshopProductServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EshopProductServiceApplication.class, args); 
+	}
+	
+}
